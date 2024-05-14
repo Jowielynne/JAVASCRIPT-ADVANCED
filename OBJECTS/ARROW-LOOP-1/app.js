dfd48c31e66fccef553dd16fb4d1404225cf78f1
Jowielynne <9023130@student.zadkine.nl>
2024-05-14 10:29:10 +0200

    arrow loop

---
+++ OBJECTS/ARROW-LOOP-1/app.js
@@ -0,0 +1,11 @@
+// ARRAY, MARVELS.
+const marvels = ["Doctor Strange", "Black Panther", "Iron Man", "GOTG"];
+
+// ARROW FUNCTION.
+const addMarvel = () => {
+    marvels.push('Thor') 
+};
+
+addMarvel()
+console.log(marvels)
+
