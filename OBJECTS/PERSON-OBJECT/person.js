56ff26627eae3135d692cb06578bcd61a22fa66b
Jowielynne <9023130@student.zadkine.nl>
2024-05-31 10:29:39 +0200

    person object
    jason derulo

--- OBJECTS/PERSON-OBJECT/person.js
+++ OBJECTS/PERSON-OBJECT/person.js
@@ -2,21 +2,12 @@ let output = document.querySelector('.output');
 let out = '';
 
 const person = {
-<<<<<<< Updated upstream
-    name: 'a',
-    age: 'a',
-    dob: 'a',
-    gender: 'a',
-    status: 'a',
-    hobbies: [ 'help' , '2', '3', '4' ],
-=======
     name: 'Shrek',
     age: 44,
     dob: 'December 24 1391',
     gender: 'Male',
     alive: true,
     hobbies: [ 'bullying kids' , '2', '3', '4' ],
->>>>>>> Stashed changes
     changeName: function (newName){
        this.name = newName
     },
